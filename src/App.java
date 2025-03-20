50cbbf6c75501d213b5b7951ae9ffde6a3ee25fc
rustleupchef <rustle.up.chef@gmail.com>
2025-03-19 20:14:40 -0500

    grab json items

--- src/App.java
+++ src/App.java
@@ -1,4 +1,24 @@
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        JSONObject config = (JSONObject) new JSONParser().parse(new FileReader("config.json"));
+        
+        String[] from = jsonArraytoStringArray(config.get("from"));
+        String[] to = jsonArraytoStringArray(config.get("to"));
+        String[] targets = jsonArraytoStringArray(config.get("targets"));
+    }
+
+    private static String[] jsonArraytoStringArray(Object objectArray) {
+        JSONArray jsonArray = (JSONArray) objectArray;
+        String[] stringArray = new String[jsonArray.size()];
+        for (int i = 0; i < jsonArray.size(); i++) {
+            stringArray[i] = (String) jsonArray.get(i);
+        }
+        return stringArray;
     }
 }
