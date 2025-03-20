f9f0e69dfda3c1575e4df13c26c26bedee62e4cc
rustleupchef <rustle.up.chef@gmail.com>
2025-03-19 20:33:18 -0500

    added debugging functions, and filtered arrays

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
+import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -7,10 +11,14 @@ import org.json.simple.parser.JSONParser;
 public class App {
     public static void main(String[] args) throws Exception {
         JSONObject config = (JSONObject) new JSONParser().parse(new FileReader("config.json"));
-        
+
         String[] from = jsonArraytoStringArray(config.get("from"));
         String[] to = jsonArraytoStringArray(config.get("to"));
-        String[] targets = jsonArraytoStringArray(config.get("targets"));
+        String[] exceptions = jsonArraytoStringArray(config.get("exceptions"));
+
+        from = filterPaths(from);
+        to = filterPaths(to);
+        exceptions = filter(false, exceptions);
     }
 
     private static String[] jsonArraytoStringArray(Object objectArray) {
@@ -20,5 +28,41 @@ public class App {
             stringArray[i] = (String) jsonArray.get(i);
         }
         return stringArray;
+    }
+
+    private static String[] filterPaths(String[] paths) {
+        return filter(true, paths);
+    }
+
+    private static String[] filter(boolean isPath, String[] array) {
+        if (isPath) {
+            List<String> paths = new ArrayList<String>();
+            HashSet<String> unique = new HashSet<String>();
+            for (String path : array) {
+                if (!new File(path).exists() || !new File(path).isDirectory())
+                    continue;
+                if (unique.add(path)) {
+                    paths.add(path);
+                }
+            }
+            return paths.toArray(new String[0]);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            String trimed = array[i];
+            if (trimed.length() == 0)
+                continue;
+            if (trimed.charAt(0) == '.')
+                trimed = trimed.substring(1);
+            array[i] = trimed;
+        }
+        return array;
+    }
+
+    private static void printArray(String[] array) {
+        for (String string : array) {
+            System.out.print(string + ", ");
+        }
+        System.out.println();
     }
 }
