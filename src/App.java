7b7de5ac9230ac00876049fb446d7a53ba7d9d98
rustleupchef <rustle.up.chef@gmail.com>
2025-03-24 22:22:35 -0500

    changed message

--- src/App.java
+++ src/App.java
@@ -28,17 +28,17 @@ public class App {
         to = filterPaths(to);
 
         if (to.length == 0) {
-            JOptionPane.showMessageDialog(null, "Invalid Config.json. To is not filled out", "Problem", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Invalid Config.json. To is not filled out", "Error", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
 
         if (from.length == 0) {
-            JOptionPane.showMessageDialog(null, "Invalid Config.json. From is not filled out", "Problem", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Invalid Config.json. From is not filled out", "Error", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
 
         if (model.equals("")) {
-            JOptionPane.showMessageDialog(null, "Invalid Config.json. Model is not filled out", "Problem", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Invalid Config.json. Model is not filled out", "Error", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
 
